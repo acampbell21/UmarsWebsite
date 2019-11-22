f09e49d7455c076a11da736afc97449e9c754fbb
Alexander Campbell <acampbell2231@gmail.com>
2019-11-22 01:00:09 -0800

    Prepare for deploy

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var path = require('path');
 
 var index = require('./routes/index');
 const app = express();
@@ -53,7 +54,15 @@ module.exports = app;
 
 app.use('/', index);
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to Umars API' }));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
