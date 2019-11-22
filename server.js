dee640949c1b451d9e62713eb1df671d97fa33c3
Alexander Campbell <acampbell2231@gmail.com>
2019-11-22 05:22:59 -0800

    :525 am boys

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ module.exports = app;
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('/*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
