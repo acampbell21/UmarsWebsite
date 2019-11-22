6dc9e06596ee0255ff58dcb5d105a504b73e8922
Alexander Campbell <acampbell2231@gmail.com>
2019-11-22 04:47:10 -0800

    untracked node modules

--- server.js
+++ server.js
@@ -52,9 +52,9 @@ module.exports = app;
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'build')));
 
-  app.get('*', (req, res) =>
+  app.get('/*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
