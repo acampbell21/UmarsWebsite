a466848df3e253599b8d884fb7be7ec2f1fe8d47
Alexander Campbell <acampbell2231@gmail.com>
2019-11-22 03:54:06 -0800

    fuck my life

--- server.js
+++ server.js
@@ -18,10 +18,6 @@ app.use(function(req, res, next) {
   next();
 });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, './client/public', 'favicon.ico')));
 app.use(logger('dev'));
